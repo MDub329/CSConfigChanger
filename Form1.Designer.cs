abab549d898c5241559aff988aa172d81ae6d672
Matt <MDub329@gmail.com>
2018-06-26 23:19:54 -0500

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace CSConfigChanger2._0
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.cfgButton = new System.Windows.Forms.Button();
+      this.crossHair1 = new System.Windows.Forms.Button();
+      this.crossHair2 = new System.Windows.Forms.Button();
+      this.crossHair3 = new System.Windows.Forms.Button();
+      this.exit = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // cfgButton
+      // 
+      this.cfgButton.Location = new System.Drawing.Point(150, 18);
+      this.cfgButton.Name = "cfgButton";
+      this.cfgButton.Size = new System.Drawing.Size(207, 72);
+      this.cfgButton.TabIndex = 0;
+      this.cfgButton.Text = "Config";
+      this.cfgButton.UseVisualStyleBackColor = true;
+      this.cfgButton.Click += new System.EventHandler(this.cfgButton_Click);
+      // 
+      // crossHair1
+      // 
+      this.crossHair1.Location = new System.Drawing.Point(150, 96);
+      this.crossHair1.Name = "crossHair1";
+      this.crossHair1.Size = new System.Drawing.Size(207, 72);
+      this.crossHair1.TabIndex = 1;
+      this.crossHair1.Text = "Crosshair #1";
+      this.crossHair1.UseVisualStyleBackColor = true;
+      this.crossHair1.Click += new System.EventHandler(this.crossHair1_Click);
+      // 
+      // crossHair2
+      // 
+      this.crossHair2.Location = new System.Drawing.Point(150, 174);
+      this.crossHair2.Name = "crossHair2";
+      this.crossHair2.Size = new System.Drawing.Size(207, 72);
+      this.crossHair2.TabIndex = 2;
+      this.crossHair2.Text = "Crosshair #2";
+      this.crossHair2.UseVisualStyleBackColor = true;
+      this.crossHair2.Click += new System.EventHandler(this.crossHair2_Click);
+      // 
+      // crossHair3
+      // 
+      this.crossHair3.Location = new System.Drawing.Point(150, 252);
+      this.crossHair3.Name = "crossHair3";
+      this.crossHair3.Size = new System.Drawing.Size(207, 72);
+      this.crossHair3.TabIndex = 3;
+      this.crossHair3.Text = "Crosshair #3";
+      this.crossHair3.UseVisualStyleBackColor = true;
+      this.crossHair3.Click += new System.EventHandler(this.crossHair3_Click);
+      // 
+      // exit
+      // 
+      this.exit.Location = new System.Drawing.Point(150, 330);
+      this.exit.Name = "exit";
+      this.exit.Size = new System.Drawing.Size(207, 72);
+      this.exit.TabIndex = 4;
+      this.exit.Text = "Exit";
+      this.exit.UseVisualStyleBackColor = true;
+      this.exit.Click += new System.EventHandler(this.exit_Click);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(518, 431);
+      this.Controls.Add(this.exit);
+      this.Controls.Add(this.crossHair3);
+      this.Controls.Add(this.crossHair2);
+      this.Controls.Add(this.crossHair1);
+      this.Controls.Add(this.cfgButton);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button cfgButton;
+    private System.Windows.Forms.Button crossHair1;
+    private System.Windows.Forms.Button crossHair2;
+    private System.Windows.Forms.Button crossHair3;
+    private System.Windows.Forms.Button exit;
+  }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+
+namespace CSConfigChanger2._0
+{
+  public partial class Form1 : Form
+  {
+
+    const string CSCFGFile = @"B:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\csgo\cfg\config.cfg";
+    const string CSCrossFile1 = @"B:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\csgo\cfg\CSCrosshair1.cfg";
+    const string CSCrossFile2 = @"B:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\csgo\cfg\CSCrosshair1.cfg";
+    const string CSCrossFile3 = @"B:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\csgo\cfg\CSCrosshair1.cfg";
+
+    const string ExternalCFGFile = @"F:\Not School Stuff\CS Stuff\config.cfg";
+    const string ExternalCrossFile1 = @"F:\Not School Stuff\CS Stuff\CSCrosshair1.cfg";
+    const string ExternalCrossFile2 = @"F:\Not School Stuff\CS Stuff\CSCrosshair2.cfg";
+    const string ExternalCrossFile3 = @"F:\Not School Stuff\CS Stuff\CSCrosshair3.cfg";
+
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+  
+    private void SaveFiles(String cSFile, String externalFile)
+    {
+      this.Visible = false;
+      var process = Process.Start("notepad.exe", cSFile);
+      process.WaitForExit();
+      File.Copy(cSFile, externalFile, true);
+      this.Visible = true;
+    }
+
+    private void cfgButton_Click(object sender, EventArgs e)
+    {
+      SaveFiles(CSCFGFile, ExternalCFGFile);
+    }
+
+    private void crossHair1_Click(object sender, EventArgs e)
+    {
+      SaveFiles(CSCrossFile1, ExternalCrossFile1);
+    }
+
+    private void crossHair2_Click(object sender, EventArgs e)
+    {
+      SaveFiles(CSCrossFile2, ExternalCrossFile2);
+    }
+
+    private void crossHair3_Click(object sender, EventArgs e)
+    {
+      SaveFiles(CSCrossFile3, ExternalCrossFile3);
+    }
+
+    private void exit_Click(object sender, EventArgs e)
+    {
+      Application.Exit();
+    }
+  }
+}
